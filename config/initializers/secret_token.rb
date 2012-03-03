e0c8237785db8c5c19ce70ad30547b73c8f69d24
unknown <szelpe@.(none)>
2012-03-03 16:29:58 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nyilvantartas::Application.config.secret_token = '542203970ab0c7e57d3a3162b3775c63a1e08ad95e1ca8e3d3a8560b998c8a6400ae86b1e920948f5993ce1236930c1965f96b5994f7d3f7bc5a61ac177f2a5b'
